2d721d4dff0a6463a289dd79ed0d5cad305e6aeb
jkahrs <jkahrs@iu.edu>
2016-11-07 21:43:50 -0500

    initial extraction working, scrolling needs work

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Nav from './components/nav/Nav';
+import Intro from './components/intro/Intro';
+import Portfolio from './components/portfolio/Portfolio';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Project Chickens</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Nav/>
+          <Intro/>
+          <Portfolio/>
+          <About/>
+          <Contact/>
+          <Footer/>
       </div>
     );
   }
